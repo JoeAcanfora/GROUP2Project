e30bbcb0030b8a45b7023ee6c9facf1e36be69e2
Joe Acanfora <joeaca04@gmail.com>
2014-09-21 19:58:35 -0400

    Homework 3
    Let's each do different visualizations.  I'll do summarizations for the
    first 20 variables, and if each of you want to take a set of variables
    to summarize that would be helpful!

---
+++ Acanfora_HW3.R
@@ -0,0 +1,88 @@
+# Joe Acanfora
+# 8/27/2014
+# CMDA
+# Homework 3
+
+setwd("C://Users//Joe//Documents//~Virginia Tech//Computer Science//CDMA 3654//hw3")
+vehicleData <- read.csv("vehicles.csv", header=TRUE, sep=",")
+
+#numeric variables
+# 0 barrels08
+# 1 barrelsA08
+# 2 charge120
+# 3 charge240
+# 4 city08
+# 5 city08U
+# 6 cityA08
+# 7 cityA08U
+# 8 cityCD
+# 9 cityE
+# 10 cityUF
+# 11 co2
+# 12 co2A
+# 13 co2TailpipeAGpm
+# 14 co2TailpipeGpm
+# 15 comb08
+# 16 comb08U
+# 17 combA08
+# 18 combA08U
+# 19 combE
+# 20 combinedCD
+# 13 combinedUF
+# 14 cylinders
+# 15 displ
+# 16 drive
+# 17 engId
+# 18 eng_dscr
+# 19 feScore
+# 20 fuelCost08
+# 21 fuelCostA08
+# 22 fuelType
+# 23 fuelType1
+# 24 ghgScore
+# 25 ghgScoreA
+# 26 highway08
+# 27 highway08U
+# 28 highwayA08
+# 29 highwayA08U
+# 30 highwayCD
+# 31 highwayE
+# 32 highwayUF
+# 33 hlv
+# 34 hpv
+# 35 id
+# 36 lv2
+# 37 lv4
+# 38 pv2
+# 39 pv4
+# 40 range
+# 41 rangeCity
+# 42 rangeCityA
+# 43 rangeHwy
+# 44 rangeHwyA
+# 45 UCity
+# 46 UCityA
+# 47 UHighway
+# 48 UHighwayA
+# 49 year
+# 50 youSaveSpend
+
+# non-numeric 
+# 1 make
+# 2 model
+# 3 mpgData
+# 4 phevBlend
+# 5 trany
+# 6 guzzler - incomplete the blank data should be F or false
+# 7 trans_scr - incomplete
+# 8 tCharger - incomplete the blank data should be F or false
+# 9 sCharger - incomplete the blank data should be F or false
+# 10 atvType - incomplete the blank data should be "gas" or "standard"
+# 11 fuelType2 - incomplete
+# 12 rangeA - incomplete
+# 13 evMotor - incomplete
+# 14 mfrCode - incomplete
+
+
+
+
